458021d5b4db992090ad895cc92ce8aef3327bf9
Feaelin Moilar <feaelin@kemenel.org>
2005-03-08 19:24:40 +0000

    *** empty log message ***

---
+++ src/inc/heartbeat.h
@@ -0,0 +1,14 @@
+/*  
+ *  heartbeat.h
+ *  $Revision: 1.1 $ $Date: 2005/03/08 19:24:40 $
+ *  Refer to heartbeat.c for a detailed explanation.
+ *
+ */
+
+/*
+ * $Log: heartbeat.h,v $
+ * Revision 1.1  2005/03/08 19:24:40  feaelin
+ * *** empty log message ***
+ *
+ */
+int heartbeat(void);
