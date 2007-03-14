6d14dc336aea853a5a0251885ad17912395582c8
Feaelin Moilar <feaelin@kemenel.org>
2007-03-14 00:29:09 +0000

    Fixed grammatical errors

--- src/heartbeat.c
+++ src/heartbeat.c
@@ -1,6 +1,6 @@
 /*  
  *  heartbeat.c
- *  $Revision: 1.2 $ $Date: 2005/03/08 18:57:36 $
+ *  $Revision: 1.3 $ $Date: 2007/03/14 00:29:09 $
  *  We can pretend that MUF provides us a heartbeat through the use of a MUF
  *  dispatcher program in conjunction with process backgrounding. The result
  *  is less than desired, however. Adding a true heartbeat which can execute
@@ -10,6 +10,9 @@
  */
 /*
  * $Log: heartbeat.c,v $
+ * Revision 1.3  2007/03/14 00:29:09  feaelin
+ * Fixed grammatical errors
+ *
  * Revision 1.2  2005/03/08 18:57:36  feaelin
  * Added the heartbeat modifications. You can add programs to the @heartbeat
  * propdir and the programs will be executed every 15 seconds.
@@ -40,7 +43,7 @@
 int heartbeat (void)
 {
   if (!OkObj(tp_heartbeat_user)) {
-    log_status("ERRO: Invalid database item #%d set as the heartbeat_user. Automatically turning heartbeat off.",
+    log_status("ERROR: Invalid database item #%d set as the heartbeat_user. Automatically tuning heartbeat off.",
 	       tp_heartbeat_user);
     tune_setparm((dbref) 1, "heartbeat", "no");
     return(0);
